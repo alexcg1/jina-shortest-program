10080313de1ab68e11390019645586f6615e1391
Alex C-G <alexcg@outlook.com>
2021-06-30 15:25:07 +0200

    fix: jina hub syntax

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import jina
-flow = jina.Flow(port_expose=45678, protocol='http').add(uses="jinahub://encoder").add(uses="jinahub://indexer")
+flow = jina.Flow(port_expose=45678, protocol='http').add(uses="jinahub+docker://encoder").add(uses="jinahub+docker://indexer")
 with flow:
     flow.post(on="/index", inputs=jina.types.document.generators.from_csv('data/input.csv'))
     flow.block()
