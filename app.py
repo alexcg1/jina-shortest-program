23077aa4c68e558322a3b6c6e480545ced6f9574
Alex C-G <alexcg@outlook.com>
2021-07-16 11:21:31 +0200

    try fixing from_csv

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from jina import Flow
 from jina.types.document.generators import from_csv
 from jinahub.indexers.simple import SimpleIndexer
+import sys
+
+docs = (from_csv("data/community.csv", field_resolver={"title": "text"}),)
+# print(next(docs))
 
 flow = (
     Flow(port_expose=45678, protocol="http")
-    .add(uses="jinahub+docker://TransformerTorchEncoder", pretrained_model_or_path="sentence-transformers/msmarco-distilbert-base-v3")
+    .add(
+        uses="jinahub+docker://TransformerTorchEncoder",
+        name="encoder",
+        # override_with={
+        # "pretrained_model_or_path": "sentence-transformers/msmarco-distilbert-base-v3"
+        # },
+    )
     .add(
         uses=SimpleIndexer,
-        dump_path="workspace",
-        override_with={"index_file_name": "index.json"},
+        name="indexer",
+        override_with={"index_file_name": "index.json", "dump_path": "workspace"},
     )
 )
 with flow:
-    flow.post(on="/index", inputs=from_csv("data/input.csv"))
+    flow.post(
+        on="/index",
+        inputs=from_csv("data/community_20.csv", field_resolver={"title": "content"}),
+    )
     flow.block()
