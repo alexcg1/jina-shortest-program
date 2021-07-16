2dfe3602b4a72f167bed5e4eeb2acb62479f8af4
Alex C-G <alexcg@outlook.com>
2021-07-16 10:44:08 +0200

    refactor: use hub

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-import jina
-flow = jina.Flow(port_expose=45678, protocol='http').add(uses="jinahub+docker://encoder").add(uses="jinahub+docker://indexer")
+from jina import Flow
+from jina.types.document.generators import from_csv
+from jinahub.indexers.simple import SimpleIndexer
+
+flow = (
+    Flow(port_expose=45678, protocol="http")
+    .add(uses="jinahub+docker://TransformerTorchEncoder", pretrained_model_or_path="sentence-transformers/msmarco-distilbert-base-v3")
+    .add(
+        uses=SimpleIndexer,
+        dump_path="workspace",
+        override_with={"index_file_name": "index.json"},
+    )
+)
 with flow:
-    flow.post(on="/index", inputs=jina.types.document.generators.from_csv('data/input.csv'))
+    flow.post(on="/index", inputs=from_csv("data/input.csv"))
     flow.block()
