af07e9f8497ce341d79f112b150b61f4e62dc761
Alex C-G <alexcg@outlook.com>
2021-07-22 16:38:28 +0200

    refactor: rm args; re-do one-liner

--- sensible.py
+++ sensible.py
@@ -1,25 +1,25 @@
 from jina import Flow
 from jina.types.document.generators import from_csv
-from jinahub.indexers.simple import SimpleIndexer
 
-with open("data/community_20.csv") as fp:
-    docs = list(from_csv(fp, field_resolver={"title": "text"}))
+# Open our data CSV
+with open("data/community_20.csv") as file:
+    # Create a DocumentArray from the CSV, choosing "title" as the field to encode and index
+    docs = list(from_csv(file, field_resolver={"title": "text"}))
 
+# Create a Flow. This is a pipeline that takes a DocumentArray as input, processes it, and returns a different DocumentArray as output
 flow = (
-    Flow(port_expose=45678, protocol="http")
+    Flow(port_expose=45678, protocol="http") # Set up REST gateway for searching
     .add(
-        uses="jinahub+docker://TransformerTorchEncoder",
+        uses="jinahub+docker://TransformerTorchEncoder", # Add an encoder. This is the neural net that "understands" your data, downloaded from Jina Hub
         name="encoder",
-        override_with={
-            "pretrained_model_name_or_path": "sentence-transformers/msmarco-distilbert-base-v3"
-        },
     )
     .add(
-        uses=SimpleIndexer,
+        uses="jinahub+docker://SimpleIndexer", # Add an indexer. This creates a searchable index of the encodings and metadata
         name="indexer",
-        override_with={"index_file_name": "index.json", "dump_path": "workspace"},
     )
 )
+
+# Start the Flow
 with flow:
-    flow.post(on="/index", inputs=docs)
-    flow.block()
+    flow.post(on="/index", inputs=docs) # Set the Flow to index
+    flow.block() # Keep the Flow open, ready for user to search
